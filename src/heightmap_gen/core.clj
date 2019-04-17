d52418c32498694baaa9b1b01d2eb0cdc4b42d05
Brandon Smith <brandon.smith.945@gmail.com>
2019-04-16 22:33:04 -0500

    Initialized leinegin project

---
+++ src/heightmap_gen/core.clj
@@ -0,0 +1,6 @@
+(ns heightmap-gen.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/heightmap_gen_2/core_test.clj
@@ -0,0 +1,7 @@
+(ns heightmap-gen.core-test
+  (:require [clojure.test :refer :all]
+            [heightmap-gen.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
